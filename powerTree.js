a4d46996f8d9218b24181ab7e9226d769e9e2ea0
Vitaly Kuklin <vitalykuklin@gmail.com>
2018-08-07 12:08:48 +0700

    Add files via upload

--- powerTree.js
+++ powerTree.js
@@ -2,10 +2,14 @@ const powerTree = num => {
   while (num >= 1) {
     if (num === 1) {
       return 1;
-    } else {
-      num = powerTree(num / 3);
-    }
+    } 
+    num = powerTree(num / 3);
   }
   return 0;
 };
 const power = (n) => (powerTree(n) === 1) ? true : false;
+//export default power;
+//powerTree(3);
+//powerTree(1); // true (3^0)
+//powerTree(2); // false
+power(9); // true (3^2)
