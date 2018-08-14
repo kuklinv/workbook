1668173811688791fa82df89789b1b39ed320a54
Vitaly Kuklin <vitalykuklin@gmail.com>
2018-08-14 18:13:07 +0700

    Add files via upload

---
+++ _logic.js
@@ -0,0 +1,17 @@
+// каррирование
+/*
+If(True)('one')('two');  // one
+If(False)('one')('two'); // two
+
+If(func)('one')('two'); // ?
+
+const ConditionFunction = If(True);
+ConditionFunction('one')('two'); // one
+
+const ConditionFunction = If(False);
+ConditionFunction('one')('two'); // two
+*/
+
+export const If = f => one => two => f(one)(two); 
+export const True = one => two => one;
+export const False = one => two => two;
