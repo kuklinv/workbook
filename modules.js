4b8a1c29fa6fc1ace9f0afef8529824a8e6f2ddd
Vitaly Kuklin <vitalykuklin@gmail.com>
2018-10-05 17:22:36 +0700

    Add files via upload

---
+++ modules.js
@@ -0,0 +1,27 @@
+const bigLettersCount = (str) => {
+  // BEGIN (write your solution here)
+var counter = 0;
+for (var i = 0; i < length(str); i += 1) {
+    if
+}
+  // END
+};
+
+
+const compare = (first, second) => {
+  const firstCount = bigLettersCount(first);
+  const secondCount = bigLettersCount(second);
+
+  // BEGIN (write your solution here)
+
+  // END
+};
+
+const greaterThan = (first, second) =>
+  compare(first, second) === 1;
+
+const lessThan = (first, second) =>
+  compare(first, second) === -1;
+
+const isEqual = (first, second) =>
+  compare(first, second) === 0;
