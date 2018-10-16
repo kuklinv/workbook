fa69ef3d878c994a3af6c195b2ba780ccfa2af7f
Vitaly Kuklin <vitalykuklin@gmail.com>
2018-10-16 17:35:23 +0700

    Add files via upload

--- isHappyNumber.js
+++ isHappyNumber.js
@@ -1,24 +1,14 @@
 //функцию, которая должна вернуть true, если число счастливое, и false, если нет.
 // Количество итераций процесса поиска необходимо ограничить числом 10.
-const isHappyNumber = (num) => {
-//var numString = num.toString(10);
-  for (let i = 1; i <= 10; i += 1) {
-    function getNumbersSum(num){
-      return num.toString().split('').reduce(function(a, b) {
-        return +a + +b;
-      });
-    }
-  }
+function isHappyNumber(num) {
+for (var i = 0; i <= 10; i += 1) {
+ if (sumOfSquareDigits(num) !== 1 && sumOfSquareDigits(num).toString().length < 2) {
+     return false;
+ } else  return true;
+}
 };
 
-/*7   => 7^2 = 49,
-49  => 4^2 + 9^2 = 16 + 81 = 97,
-97  => 9^2 + 7^2 = 81 + 49 = 130,
-130 => 1^2 + 3^2 + 0^2 = 10,
-10  => 1^2 + 0^2 = 1.
-*/
-
-const sumOfSquareDigits = (num) => {
+function sumOfSquareDigits(num) {
   const numAsStr = String(num);
   let sum = 0;
   for (let i = 0; i < numAsStr.length; i += 1) {
@@ -26,4 +16,12 @@ const sumOfSquareDigits = (num) => {
     sum += digit * digit;
   }
   return sum;
-};
+}
+
+export default isHappyNumber;
+/*7   => 7^2 = 49,
+49  => 4^2 + 9^2 = 16 + 81 = 97,
+97  => 9^2 + 7^2 = 81 + 49 = 130,
+130 => 1^2 + 3^2 + 0^2 = 10,
+10  => 1^2 + 0^2 = 1.
+*/
