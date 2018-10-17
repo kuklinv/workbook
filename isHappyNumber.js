0402b916641402c20e14d2f635d932fb3c212ce6
Vitaly Kuklin <vitalykuklin@gmail.com>
2018-10-17 14:19:58 +0700

    Add files via upload

--- isHappyNumber.js
+++ isHappyNumber.js
@@ -1,22 +1,27 @@
 //функцию, которая должна вернуть true, если число счастливое, и false, если нет.
 // Количество итераций процесса поиска необходимо ограничить числом 10.
-function isHappyNumber(num) {
-for (var i = 0; i <= 10; i += 1) {
- if (sumOfSquareDigits(num) !== 1 && sumOfSquareDigits(num).toString().length < 2) {
-     return false;
- } else  return true;
-}
-};
-
-function sumOfSquareDigits(num) {
-  const numAsStr = String(num);
+const sumOfSquareDigits = (num) => {
   let sum = 0;
+  let numAsStr = String(num);
+
   for (let i = 0; i < numAsStr.length; i += 1) {
     const digit = Number(numAsStr[i]);
     sum += digit * digit;
   }
+
   return sum;
-}
+};
+
+const isHappyNumber = (num) => {
+  let number = num;
+  for (let i = 0; i < 10; i += 1) {
+    number = sumOfSquareDigits(number);
+    if (number === 1) {
+      return true;
+    }
+  }
+  return false;
+};
 
 export default isHappyNumber;
 /*7   => 7^2 = 49,
@@ -25,3 +30,8 @@ export default isHappyNumber;
 130 => 1^2 + 3^2 + 0^2 = 10,
 10  => 1^2 + 0^2 = 1.
 */
+/*for (var i = 0; i <= 10; i += 1) {
+ if (sumOfSquareDigits(num) === 1 && sumOfSquareDigits(num).toString().length < 2) {
+     return true;
+ } else  return true;
+}*/
