9f5508c8c3ceeedd402e16d2734974620f0e011a
kuklinv <vitalykuklin@gmail.com>
2018-10-19 20:04:39 +0700

    add

--- addDigitsRec.js
+++ addDigitsRec.js
@@ -1,13 +1,3 @@
-// принимает неотрицательное число. расчитывает сумму всех входящих в него
-// цифр пока не останеться одно
-function sumOfDigits(number) {    
-    return number.toString().split('').reduce(function(a,b){return +a + +b}, 0);
-};
-
-
-
-// другое решение через рекурсию и отдельную функцию сложения
-
 const add = (num) => {
   const numToStr = String(num);
   let result = 0;
