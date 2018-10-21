6468e34ccd95f710183c2fde3772dcdf05b935bf
kuklinv <vitalykuklin@gmail.com>
2018-10-21 20:09:13 +0700

    work with lists

---
+++ list.js
@@ -0,0 +1,14 @@
+const has = (numbers, num) => {
+
+};
+const reverse = (numbers) => {
+
+};
+const copy = (numbers) => {
+
+};
+const concat = (numbers, numbers2) => {
+
+};
+
+export { has, reverse, concat };
