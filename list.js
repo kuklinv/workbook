ec7924c2947bd75cdeb96b45182f42e8a03bb4ec
kuklinv <vitalykuklin@gmail.com>
2018-10-23 08:25:11 +0700

    add lists modification

--- list.js
+++ list.js
@@ -1,4 +1,11 @@
 const has = (numbers, num) => {
+var inputList = numbers;
+var elementMember = num;
+var toStrInputNumbers = toString(inputList);
+
+//while (a !== null) {        // цикл перебоа элементов списка до его окончания (пока последняя пара)
+//  return tail (a);          // не null во втором элементе
+//};                           
 
 };
 const reverse = (numbers) => {
