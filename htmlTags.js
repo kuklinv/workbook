9d6fadd5371b5f0cd31292dc60f5c5acfc2d5078
Vitaly Kuklin <vitalykuklin@gmail.com>
2018-10-26 16:08:07 +0700

    Add files via upload

--- htmlTags.js
+++ htmlTags.js
@@ -1,18 +1,17 @@
-const node = (name, value) => cons (name, cons(value, null));
+const node = (name, value) => consList(name, consList(value, null));
 const name = (tag) => head(tag);
 const value = (tag) => head(tail(tag));
-
-/*const append = (dom, tag) => {
-  if (isEmpty(dom)) {
-    return tag;
-  }  
-  return cons(head(dom), append(tail(dom), tag));
-};
-*/
-const append = (dom, tag) => cons(name(tag), cons(value, cons(dom, null)));
+const append = (dom, tag) => consList(tag, dom);
 
 const toString = (dom) => {
-
+  const iter = (dom, acc) => {
+    if(isEmpty(dom)) {
+  	  return acc;
+    }
+    return `${toString(tail(dom))}<${name(head(dom))}>${value(head(dom))}</${name(head(dom))}>${acc}` 
+  }
+  return iter(dom, ``);
 };
+
 
 export { node, name, value, append, toString };
