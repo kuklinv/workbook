504cf96abca962dc02df51bbe6f44ee73f70238e
kuklinv <vitalykuklin@gmail.com>
2019-04-30 16:28:46 +0700

    addDigits to basicCoding hexlet

--- HEXLET/basicCoding.js
+++ HEXLET/basicCoding.js
@@ -3,20 +3,44 @@
 function calc(operbin, a, b) {
     switch (operbin) {
         case '+':
-            a + b;
+            return a + b;
             break;
         case '-':
-            a - b;
+        return a - b;
             break;
         case '/':
-            a / b;;
+        return a / b;;
             break;
         case '*':
-            a * b;;
+        return a * b;;
             break;
         default:
-            NaN;
+        return NaN;
     }
 }
 
 export default calc;
+
+// add Digits  - дает сумму числа пока число больше двух знаков.
+const sumDigits = (strOfNum) => {
+    let sumNum = 0;
+    for (let i = 0; i < length(strOfNum); i += 1) {
+      sumNum += Number(strOfNum[i]);
+    }
+    return sumNum;
+  };
+  
+  const addDigits = (num) => {
+    let result;
+    if (length(String(num)) < 2) {
+      result = num;
+    }
+    while (length(String(sumDigits(num))) > 1) {
+      result = sumDigits(num);
+    }
+    return result;
+  };
+  
+  export default addDigits;
+
+  
