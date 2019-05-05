cbd1e0ac20b73ed1a15c75b99aa9144e13067329
kuklinv <vitalykuklin@gmail.com>
2019-05-05 17:39:57 +0700

    add codecamp exersises

--- freeCodeCamp/introductionToJavaScript.js
+++ freeCodeCamp/introductionToJavaScript.js
@@ -1,1 +1,55 @@
 // first part of Javascript Algorithms And Data Structures Certification (300 hours)
+// Using Objects for Lookups:
+function phoneticLookup(val) {
+    var result = "";
+
+    var lookup = {
+        alpha: "Adams",
+        bravo: "Boston",
+        charlie: "Chocago",
+        delta: "Denver",
+        echo: "Easy",
+        foxtrot: "Frank"
+    };
+
+    result = lookup.alpha(val); // ore result = lookup[val];
+    /* the same:
+    switch (val) {
+        case "alpha":
+            result = "Adams";
+            break;
+        case "bravo":
+            result = "Boston";
+            break;
+        case "charlie":
+            result = "Chicago";
+            break;
+        case "delta":
+            result = "Denver";
+            break;
+        case "echo":
+            result = "Easy";
+            break;
+        case "foxtrot":
+            result = "Frank";
+    }
+
+    */
+    return result;
+}
+phoneticLookup("charlie");
+
+//////////////////////////////////////////////// checkout proprty exist in obj; 
+// Setup
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+    // Your Code Here
+    return myObj.hasOwnProperty(checkProp) ? myObj[checkProp] : "Not Found";
+}
+// Test your code by modifying these values
+checkObj("gift");
