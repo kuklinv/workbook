5f130b6f1de90896d57d9611ea567c8ac5a5cd08
kuklinv <vitalykuklin@gmail.com>
2019-05-11 12:25:29 +0700

    add new ES^ task

--- freeCodeCamp/introductionToJavaScript.js
+++ freeCodeCamp/introductionToJavaScript.js
@@ -393,4 +393,124 @@ function removeFirstTwo(list) {
 const arr = removeFirstTwo(source);
 console.log(arr); // should be [3,4,5,6,7,8,9,10]
 console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
-//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////  Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+const half = (function() {
+    "use strict"; // do not change this line
+
+    // change code below this line
+    return function half({ max, min }) {
+        // use function argument destructuring
+        return (stats.max + stats.min) / 2.0;
+    };
+    // change code above this line
+
+})();
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015
+////////////////////////////////////////////////////////////Create Strings using Template Literals
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-dup-keys"]
+};
+
+function makeList(arr) {
+    "use strict";
+
+    // change code below this line
+    const resultDisplayArray = [`<li class="text-warning">${arr[0]}</li>`,
+        `<li class="text-warning">${arr[1]}</li>`,
+        `<li class="text-warning">${arr[2]}</li>
+    `
+    ];
+    // change code above this line
+
+    return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`, 
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
+///////////////////////////////////////////////////////////////////////or!!!!!!!!!!!!!!!!! use MAP
+const resultDisplayArray = arr.map(item => `<li class="text-warning">${item}</li>`);
+
+
+//////////////////////////////////////////////////////////////Write Concise Object Literal Declarations Using Simple Fields
+
+const createPerson = (name, age, gender) => {
+    "use strict";
+    // change code below this line
+    return ({ name, age, gender });
+    // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
+
+//////////////////////////////////////////////////////////////////////////ES6: Write Concise Declarative Functions with ES6
+// change code below this line
+// change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        "use strict";
+        this.gear = newGear;
+    }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+////////////////////////////////////////////////////////Use class Syntax to Define a Constructor Function
+class Vegetable {
+
+    constructor(name) {
+        "use strict";
+        /* Alter code below this line */
+        this.name = name;
+        /* Alter code above this line */
+    }
+}
+//const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
+
+////////////////////////////////////////////////////////Use getters and setters to Control Access to an Object
+class Book {
+    constructor(author) {
+            this._author = author;
+        }
+        // getter
+    get writer() {
+            return this._author;
+        }
+        // setter
+    set writer(updatedAuthor) {
+        this._author = updatedAuthor;
+    }
+}
+const lol = new Book('anonymous');
+console.log(lol.writer); // anonymous
+lol.writer = 'wut';
+console.log(lol.writer); // wut
+
+/////////////////////////////////////////////
+class Termostat {
+    "use strict";
+    constructor(temperature) {
+
+    }
+}
+//const Thermostat = makeClass();
+const thermos = new Thermostat(76); // setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in C
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C
