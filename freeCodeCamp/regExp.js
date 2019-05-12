571baddde048b0001194e148e8318b104999428f
kuklinv <vitalykuklin@gmail.com>
2019-05-12 16:18:25 +0700

    finished my basic & ES6 & start regular expression

---
+++ freeCodeCamp/regExp.js
@@ -0,0 +1,10 @@
+///////////////////////////////////////////////////Introduction to the Regular Expression Challenges
+/// us .test()
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+/////////////////////////////// using "alternation" | "or" | "|"
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+/////////////////////////////////////////
