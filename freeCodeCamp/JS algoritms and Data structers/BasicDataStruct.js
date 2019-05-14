d430ce182e091e05f0f94dc4afa201796578f3f8
kuklinv <vitalykuklin@gmail.com>
2019-05-15 06:58:11 +0700

    continue basic data structure

--- freeCodeCamp/JS algoritms and Data structers/BasicDataStruct.js
+++ freeCodeCamp/JS algoritms and Data structers/BasicDataStruct.js
@@ -1,3 +1,22 @@
 ///////////////////////////////////////////////////// Introduction to the Basic Data Structure Challenges
 
-//
+//replase items in array with splice
+function colorChange(arr, index, newColor) {
+    arr.splice(index, 1, newColor);
+    return arr;
+}
+
+let colorScheme = ['#878787', '#a08794', '#bb7e8c', '#c9b6be', '#d1becf'];
+
+colorScheme = colorChange(colorScheme, 2, '#332327');
+// we have removed '#bb7e8c' and added '#332327' in its place
+// colorScheme now equals ['#878787', '#a08794', '#332327', '#c9b6be', '#d1becf']
+
+//////////////////////////////////////  use ....spread operator for combine arrays
+
+let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
+
+let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
+// thatArray now equals ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander']
+
+////////////////////////////////////////////////////////////
