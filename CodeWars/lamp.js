5c748adf2943509975d9f4f9dde5e31b4e6eecbd
Vitaly <vitalykuklin@gmail.com>
2019-08-28 02:03:20 +0700

    lamp to codewars

---
+++ CodeWars/lamp.js
@@ -0,0 +1,31 @@
+class Lamp {
+    constructor(color) {
+        this.color = color;
+        this.on = false;
+    }
+    set color (color) {
+        return this.color = color;
+    }
+    get color () {
+        return this.color;
+    }
+    toggle_switch() {
+        if (this.on == false) {
+            return this.on == true;
+        }
+        else if (this.on == true) {
+            return this.on == false;
+        }
+    }
+
+    state() {
+        if (this.on == false) {
+            return "The lamp is off.";
+        }
+        else {
+            return = "The lamp is on.";
+        }
+    }
+}
+
+let lamp = new Lamp("Blue");
