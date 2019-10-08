cf2985238e9a3867124e7b69e6bf0691eef5d3aa
Vitaly <vitalykuklin@gmail.com>
2019-10-09 06:06:20 +0700

    Feature: clean limitSizeStream.test.js

--- nodeJS/my sandebox/jsru_module2/task1/test/LimitSizeStream.test.js
+++ nodeJS/my sandebox/jsru_module2/task1/test/LimitSizeStream.test.js
@@ -42,27 +42,3 @@ describe('2-module-1-task', () => {
         });
     });
 });
-
-const isPrime = (num) => {
-    if (num <= 1) {return false;}
-    const iter = (counter, prime) => {
-        if (counter === num) {
-            return true;
-        }
-        if (prime % counter === 0) {
-            return false;
-        }
-        return iter(counter + 1, prime);
-    };
-    return iter(2, num);
-};
-
-function result(x1, y1, x2, y2) {
-    let print = '';
-    for (let i = x1; i < y1; i += 1) {
-        if (isPrime(i)) {
-            print += i.toString() + ' ';
-        }
-    }
-    console.log(print.slice(0, -1));
-}s
