59026f3ae53cb011e5cc5f171ec90cf35d7acad2
Vitaly <vitalykuklin@gmail.com>
2019-10-11 07:46:47 +0700

    Feature: task 2 module 2 in work

--- nodeJS/jsru_nodejsCourse/tasks/2-module/2-task/LineSplitStream.js
+++ nodeJS/jsru_nodejsCourse/tasks/2-module/2-task/LineSplitStream.js
@@ -4,13 +4,17 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    // this.encoding = options.encoding;
   }
 
   _transform(chunk, encoding, callback) {
+
   }
 
   _flush(callback) {
+
   }
 }
 
 module.exports = LineSplitStream;
+
