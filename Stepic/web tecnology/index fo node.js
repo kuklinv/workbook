25068e4f7f15d4b6b0929d5aae3ebe1f428867fb
Vitaly <vitalykuklin@gmail.com>
2019-10-18 15:13:48 +0700

    Feature: add few task from Stepik and 4-modules task to nodejs ciurse

---
+++ Stepic/web tecnology/index fo node.js
@@ -0,0 +1,15 @@
+///
+let man = {
+    name: 'Vitaly',
+    surname: 'Kuklin',
+    fullName: function () {
+        return surname + ' ' + name;
+    }
+};
+class Room {
+    windows: 5,
+    owner: 'Ivanov'
+}
+
+// module.exports.man = man;
+// module.exports.Room = Room;
