5f74a3000cebdcfc5c56f7183d84f83bc22dd30b
Vitaly <vitalykuklin@gmail.com>
2019-11-01 08:50:41 +0700

    Feature: work with hexlet async programming chelenges [ref: compareFilesSizes]

--- sandbox/hexletAsync.js
+++ sandbox/hexletAsync.js
@@ -1,13 +1,31 @@
 const fs = require('fs');
 
-const myPath = './myfile';
-
-function write(path, data, cb) {
-    fs.writeFile(path, data, cb);
+function compareFileSizes(f1, f2, cb) {
+    let result;
+    fs.stat(f1, (err, stats1) => {
+        fs.stat(f2, (err, stats2) => {
+            if (stats1.size > stats2.size) {
+                result = 1;
+            } else if (stats1.size === stats2.size) {
+                result = 0;
+            } else result = -1;
+        });
+    });
+    cb();
 }
 
-write(myPath, 'world', () => {
-    console.log('success');
-});
+// Math.sign
+
+compareFileSizes('./file1', './file2', (_err, result) => console.log(result));
+
+// const myPath = './myfile';
+//
+// function write(path, data, cb) {
+//     fs.writeFile(path, data, cb);
+// }
+//
+// write(myPath, 'world', () => {
+//     console.log('success');
+// });
 
 
