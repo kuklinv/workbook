d0f53cf97441192d071d3e4ddad5b54faf0b5e65
Vitaly <vitalykuklin@gmail.com>
2019-11-01 21:46:35 +0700

    Feature: solution compare [ref: compareFilesSizes]

--- sandbox/hexletAsync.js
+++ sandbox/hexletAsync.js
@@ -4,14 +4,17 @@ function compareFileSizes(f1, f2, cb) {
     let result;
     fs.stat(f1, (err, stats1) => {
         fs.stat(f2, (err, stats2) => {
-            if (stats1.size > stats2.size) {
-                result = 1;
-            } else if (stats1.size === stats2.size) {
-                result = 0;
-            } else result = -1;
+            // console.log(stats1.size - stats2.size);
+            const  result = Math.sign(Number(stats1.size - stats2.size));
+            // console.log(result);
+            cb(null, result);
+            // if (stats1.size > stats2.size) {
+            //     result = 1;
+            // } else if (stats1.size === stats2.size) {
+            //     result = 0;
+            // } else result = -1;
         });
     });
-    cb();
 }
 
 // Math.sign
