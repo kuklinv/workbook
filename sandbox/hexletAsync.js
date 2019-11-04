b227edf61b6d183f4e06cd28def77779640f677b
Vitaly <vitalykuklin@gmail.com>
2019-11-04 18:39:43 +0700

    Feature: solution to compareFileSizes [ref: hexlet async programming. compareFileSizes]

--- sandbox/hexletAsync.js
+++ sandbox/hexletAsync.js
@@ -4,20 +4,11 @@ function compareFileSizes(f1, f2, cb) {
     let result;
     fs.stat(f1, (err, stats1) => {
         fs.stat(f2, (err, stats2) => {
-            // console.log(stats1.size - stats2.size);
             const  result = Math.sign(Number(stats1.size - stats2.size));
-            // console.log(result);
             cb(null, result);
-            // if (stats1.size > stats2.size) {
-            //     result = 1;
-            // } else if (stats1.size === stats2.size) {
-            //     result = 0;
-            // } else result = -1;
         });
     });
 }
-
-// Math.sign
 
 compareFileSizes('./file1', './file2', (_err, result) => console.log(result));
 
