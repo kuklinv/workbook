acf40bf95324092c50a4edbe0be345f0793a8299
Vitaly <vitalykuklin@gmail.com>
2019-11-07 08:21:21 +0700

    Feature: add async npm and task for hexlet course async programming

--- sandbox/hexletAsync.js
+++ sandbox/hexletAsync.js
@@ -1,19 +1,65 @@
 /////error processing
+//
 const fs = require('fs');
+const {map} = require('async');
+const path = require('path');
+const _ = require('lodash');
 
-function move(location, destination, cb) {
-    fs.readFile(location, )
+function getDirectorySize () {
+
+
+    // fs.readdir(dirPath, (error1, files) => {
+    //   if (error1) {
+    //     cb(error1);
+    //     return;
+    //   }
+    //   let filepaths = files.map(name => path.join(dirPath, name));
+    //   async.map(filepaths, fs.stat, (error2, results) => {
+    //     if (error2) {
+    //       cb(error2);
+    //       return;
+    //     }
+    //     let mapper = results.map(elem => elem.size);
+    //     _.sumBy(results, mapper);
+    //   });
+    // });
+
+
+// _.sumBy(отфильрованный массив, (item) => item.size)
+
 }
 
-// export { move };
-
-move('/opt/myfile', '/tmp/newfile', (error) => {
-    if (error) {
-        console.log('oops');
-        return;
-    }
-    console.log('yes!')
+getDirectorySize('/usr/local/bin', (err, size) => {
+    console.log(size);
 });
+
+// export { getDirectorySize };
+
+// fs.readdir - чтение содержимого директории
+// path.join - конструирует пути
+// async.map
+// fs.stat - информация о файле
+// _.sumBy - нахождение суммы в массиве
+
+/// ///////////////////////////////////////////////////////
+
+
+
+// const fs = require('fs');
+//
+// function move(location, destination, cb) {
+//     fs.readFile(location, )
+// }
+//
+// // export { move };
+//
+// move('/opt/myfile', '/tmp/newfile', (error) => {
+//     if (error) {
+//         console.log('oops');
+//         return;
+//     }
+//     console.log('yes!')
+// });
 // const fs = require('fs');
 //
 // function compareFileSizes(f1, f2, cb) {
