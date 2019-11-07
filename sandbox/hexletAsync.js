f0de0972a4a39a4bd6c807969745874949450358
Vitaly <vitalykuklin@gmail.com>
2019-11-07 08:16:12 +0700

    Feature: add 09 module at nodeJSru course material + add hexlet task - async programming

--- sandbox/hexletAsync.js
+++ sandbox/hexletAsync.js
@@ -1,16 +1,32 @@
+/////error processing
 const fs = require('fs');
 
-function compareFileSizes(f1, f2, cb) {
-    let result;
-    fs.stat(f1, (err, stats1) => {
-        fs.stat(f2, (err, stats2) => {
-            const  result = Math.sign(Number(stats1.size - stats2.size));
-            cb(null, result);
-        });
-    });
+function move(location, destination, cb) {
+    fs.readFile(location, )
 }
 
-compareFileSizes('./file1', './file2', (_err, result) => console.log(result));
+// export { move };
+
+move('/opt/myfile', '/tmp/newfile', (error) => {
+    if (error) {
+        console.log('oops');
+        return;
+    }
+    console.log('yes!')
+});
+// const fs = require('fs');
+//
+// function compareFileSizes(f1, f2, cb) {
+//     let result;
+//     fs.stat(f1, (err, stats1) => {
+//         fs.stat(f2, (err, stats2) => {
+//             const  result = Math.sign(Number(stats1.size - stats2.size));
+//             cb(null, result);
+//         });
+//     });
+// }
+//
+// compareFileSizes('./file1', './file2', (_err, result) => console.log(result));
 
 // const myPath = './myfile';
 //
