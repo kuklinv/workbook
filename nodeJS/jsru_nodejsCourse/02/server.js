84894b5dd9e655cf1b5c9f3485ee4c5856dd7cb9
Vitaly <vitalykuklin@gmail.com>
2019-11-13 07:38:45 +0700

    Feature: work with nodejsru tasks [ref: module 4/ 07.10.19]

--- nodeJS/jsru_nodejsCourse/02/server.js
+++ nodeJS/jsru_nodejsCourse/02/server.js
@@ -3,18 +3,25 @@ const fs = require('fs');
 
 const server = new http.Server();
 
-const buffer = Buffer.from('привет!');
+// const buffer = Buffer.from('привет!');
 
+let  data = '';
 server.on('request', (req, res) => {
+  req.on('data', chunk => {
+    data += chunk;
+  });
+  req.on('end', () => {
+    res.end(data);
+  })
   // console.log(buffer);
   // res.setHeader('content-type', 'text/plain; charset=utf-8');
   // res.end(buffer);
   // fs.createReadStream('big.png').pipe(res);
-  const file = fs.createReadStream('big.png');
+  // const file = fs.createReadStream('big.png');
   // file.on('data', chunk => res.write(chunk));
   // file.on('end', () => res.end());
-  file.pipe(res);
-  
+  // file.pipe(res);
+
 });
 
 server.listen(3000);
--- nodeJS/jsru_nodejsCourse/tasks/4-module/1-task/server.js
+++ nodeJS/jsru_nodejsCourse/tasks/4-module/1-task/server.js
@@ -1,23 +1,35 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+    const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+    const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'GET':
+    switch (req.method) {
+        case 'GET':
+            fs.createReadStream(filepath)
+                .on('error', err => {
+                    if (err.code === 'ENOENT') {
+                        res.statusCode = 404;
+                        res.end('file does not exist');
+                    } else {
+                        res.statusCode = 500;
+                        res.end('internal server error');
+                    }
+                })
+                .pipe(res);
+            res.end('file successfully response');
+            break;
 
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+        default:
+            res.statusCode = 501;
+            res.end('Not implemented');
+    }
 });
 
 module.exports = server;
