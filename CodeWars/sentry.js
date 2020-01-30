f5df419d860c821bb7603972f5372bea886e07fc
Vitaly <vitalykuklin@gmail.com>
2020-01-30 22:57:43 +0700

    Feature: update my dayly work [ref:codewars stepic nodeJS]

---
+++ CodeWars/sentry.js
@@ -0,0 +1,7 @@
+function sentry(year) {
+    const input = year.split(' ');
+    console.log(input);
+    //return;
+}
+
+sentry(1705);
--- nodeJS/nodejs sandebox/servertest/server.js
+++ nodeJS/nodejs sandebox/servertest/server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+    const http = require('http');
 
 const server = new http.Server();
 
