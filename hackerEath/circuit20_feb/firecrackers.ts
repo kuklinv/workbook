a1202f0f6ca0bc7eaec00b42e11bb3403026d788
Vitaly <vitalykuklin@gmail.com>
2020-02-16 21:46:50 +0700

    Feature: my code pфыыувd simple test but not passed prod mode ((( int now what wrong [ref: circuit.firecracker ]

--- hackerEath/circuit20_feb/firecrackers.ts
+++ hackerEath/circuit20_feb/firecrackers.ts
@@ -5,10 +5,7 @@ const amountsNumber = (n: string, costs: string): any => {
     let numberOfTypes = + n;
     let costsString: string[] = costs.split(' ');
     let costsArray: number[] = costsString.map((item: string) => + item);
-    // let costsArray: number[] = Array.from(costsString, item => + item);
-    // return numberOfTypes
-    console.log(numberOfTypes);
-    console.log(costsArray);
+
 }
 
 let result = amountsNumber(typesNum, costs);
--- hackerEath/circuit20_feb/index.js
+++ hackerEath/circuit20_feb/index.js
@@ -1,21 +1,40 @@
-let split = require{ 'string'};
-
-
 process.stdin.setEncoding("utf-8");
 var stdin_input = "";
 
 process.stdin.on("data", function (input) {
-    stdin_input += input;                               // Reading input from STDIN
+    let buffer = Buffer.from(input);
+    stdin_input += buffer.toString();                               // Reading input from STDIN
 });
 
 process.stdin.on("end", function () {
     main(stdin_input);
 });
 
+function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
+}
+
 function main(input) {
     let inputArg = input.split("\n");
     let numberOfTypes = + inputArg[0];
-    // let costsString = costs.split(' ');
-    let costsArray = Array.from(inputArg[1], function (item) { return +item; }).filter((item) => item != 0); // TODO: delete zeros
-    process.stdout.write("Hi, " + numberOfTypes + costsArray + ".\n");       // Writing output to STDOUT
+    let costsArray = Array.from(inputArg[1], function (item) { return +item; }).filter((item) => item !== 0);
+    ////////////////////
+    let availableAmount = [];
+    console.log(costsArray);
+    let maxCost = getMaxOfArray(costsArray);
+    for (let amount = 1; amount <= maxCost / 2; amount++) {
+        let doubleAmount = amount * 2;
+        for (let i = 0; i < numberOfTypes; i++) {
+            console.log(costsArray[i]);
+            console.log(amount);
+            console.log(doubleAmount);
+            if (costsArray[i] === amount && doubleAmount % 2 === 0 || costsArray[i] === doubleAmount && doubleAmount % 2 === 0) {
+                availableAmount.push(doubleAmount);
+            }
+            console.log(availableAmount);
+        }
+    }
+    counterOfAmount = (availableAmount.length).toString();
+    // process.stdout.write(`${counterOfAmount}`);
+    process.stdout.write(counterOfAmount);
 }
