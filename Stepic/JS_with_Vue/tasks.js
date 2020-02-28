59033555ef9405198c4443ed95f104928f8a35f0
kuklinv <vitalykuklin@gmail.com>
2020-02-28 20:43:40 +0700

    Fix: only update

--- Stepic/JS_with_Vue/tasks.js
+++ Stepic/JS_with_Vue/tasks.js
@@ -7,17 +7,32 @@
 //   return res;
 // }
 
-function beginWith(str, subs) {
-  let a = "";
-  let b = "";
-  if (subs.length == 0 || subs.length > str.length) return false;
-  let subInputStr = str.substring(0, subs.length - 1);
-  a = subs.toLowerCase();
-  b = subInputStr.toLowerCase();
-  return a == b ? true : false;
-}
+// function beginWith(str, subs) {
+//   let a = "";
+//   let b = "";
+//   if (subs.length == 0 || subs.length > str.length) return false;
+//   let subInputStr = str.substring(0, subs.length - 1);
+//   a = subs.toLowerCase();
+//   b = subInputStr.toLowerCase();
+//   return a == b ? true : false;
+// }
 // console.log(beginWith("JavScript", "java"));
 
 // let a = "Java";
 // let b = "JavaScript";
 // console.log(a == b ? true : false);
+
+function swap(map, key) {
+  for (let item of Object.keys(map)) {
+    if (map[item] == key) {
+      console.log(map[item]);
+      console.log([key]);
+      map.set('key', item);
+      map.delete(map[item]);
+    }
+  }
+  return (console.log(map));
+}
+
+console.log(swap({ "user": "Tom", "confirm": "isConfirmed" }, confirm)); // "isConfirmed"
+// console.log(swap({ "user": "Tom", "confirm": "isConfirmed" }, login)); // false
