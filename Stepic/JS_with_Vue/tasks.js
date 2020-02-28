46fa365723479cffedc9717ff5f6541b3820ecd9
Vitaly <vitalykuklin@gmail.com>
2020-02-28 08:41:06 +0700

    Feature: work with stepik tasks [string at JS&Vue]

--- Stepic/JS_with_Vue/tasks.js
+++ Stepic/JS_with_Vue/tasks.js
@@ -1,8 +1,23 @@
-function filterPersons(str) {
-  let res = "";
-  let ignore = "Sam";
-  let arrInputStr = str.split(",");
-  let filterInputArr = arrInputStr.filter(item => item !== ignore);
-  res = filterInputArr.join("");
-  return res;
+// function filterPersons(str) {
+//   let res = "";
+//   let ignore = "Sam";
+//   let arrInputStr = str.split(",");
+//   let filterInputArr = arrInputStr.filter(item => item !== ignore);
+//   res = filterInputArr.join("");
+//   return res;
+// }
+
+function beginWith(str, subs) {
+  let a = "";
+  let b = "";
+  if (subs.length == 0 || subs.length > str.length) return false;
+  let subInputStr = str.substring(0, subs.length - 1);
+  a = subs.toLowerCase();
+  b = subInputStr.toLowerCase();
+  return a == b ? true : false;
 }
+// console.log(beginWith("JavScript", "java"));
+
+// let a = "Java";
+// let b = "JavaScript";
+// console.log(a == b ? true : false);
