6feb19de0391d8c34d35ab4bb57bde3da343fe82
Vitaly <vitalykuklin@gmail.com>
2020-03-02 07:37:25 +0700

    Feature: work with stepik tasks [data and math at JS&Vue]

--- Stepic/JS_with_Vue/tasks.js
+++ Stepic/JS_with_Vue/tasks.js
@@ -39,21 +39,65 @@
 //  все значения этого массива, а затем это set вы можете присваивать ключу в мапе(внутри этого условия).
 //  После этого цикла вам нужно сконкатенировать значения в одну строку и вывести ее.
 
-function modifyMap(map) {
-  let resStr = "";
-  for (let entry in map) {
-    // console.log(entry);
-    // console.log(map[entry]);
-    if (typeof map[entry] == "object") {
-      let localSet = new Set(map[entry]);
-      map.set(entry, localSet);
-      resStr += `${entry}:${localSet};`;
-    } else {
-      map.delete(entry);
-    }
-  }
-  console.log(resStr);
+// function modifyMap(map) {
+//   let resStr = "";
+//   for (let entry in map) {
+//     // console.log(entry);
+//     // console.log(map[entry]);
+//     if (typeof map[entry] == "object") {
+//       let localSet = new Set(map[entry]);
+//       // map.set(entry, localSet);
+//       resStr += `${entry}:${localSet};`;
+//     } else {
+//       map.delete(entry);
+//     }
+//   }
+//   console.log(resStr);
+// }
+// function modifyMap(map) {
+//   let resStr = "";
+//   for (let entry in map) {
+//     if (typeof map[entry] == "object") {
+//       let localSet = new Set(map[entry]);
+//       resStr += entry + ":" + [...localSet].join("") + ";";
+//     } else map.delete(entry);
+//   }
+//   console.log(resStr);
+// }
+
+// modifyMap({ log14: [1, 2, 3, 3, 2, 1], log15: "none data", log16: ["s", "S"] }); //log14:123;log16:sS;
+// modifyMap({ log: [1, 1, 1] }); //log:1;
+
+// const data = {
+//   Day: "Monday",
+//   Month: "May",
+//   Date: 06,
+//   FullYear: 2019
+// };
+// // Mon May 06 2019
+
+// function getWeekDay(date) {
+//   const weakSet = [
+//     "Воскресенье",
+//     "Понедельник",
+//     "Вторник",
+//     "Среда",
+//     "Четверг",
+//     "Пятница",
+//     "Суббота"
+//   ];
+//   let day = date.getDay();
+//   console.log(weakSet[day]);
+// }
+
+// getWeekDay(data);
+
+function findMidlle(arr) {
+  //Ваш код
 }
 
-modifyMap({ log14: [1, 2, 3, 3, 2, 1], log15: "none data", log16: ["s", "S"] }); //log14:123;log16:sS;
-modifyMap({ log: [1, 1, 1] }); //log:1;
+findMidlle(["a", "b", "c"]); // b
+findMidlle(["1", "2", "3", "4"]); //2
+findMidlle([]); // null
+findMidlle(["a", "b", "c", "d", "e"]); //c
+findMidlle(["a", "b", "c", null, null]); // b
