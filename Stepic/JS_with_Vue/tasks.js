7b1591477d23159817cf454f796dc87f22487621
kuklinv <vitalykuklin@gmail.com>
2020-03-13 21:50:49 +0700

    Fix: only sinc with JIDE[ref: JS & VUE tasks]

--- Stepic/JS_with_Vue/tasks.js
+++ Stepic/JS_with_Vue/tasks.js
@@ -247,8 +247,9 @@ function isIsogram(str) {
   const uniq = arr => arr.filter(arrItem => arr.indexOf(arrItem));
   const unicStr = uniq(arrFromStr);
   console.log(unicStr);
-  if (str === unicStr.join("")) return true;
-  else return false;
+  // if (str === unicStr.join("")) return true;
+  // else return false;
 }
-console.log(isIsogram("hello")); //false
+isIsogram("hello");
+// console.log(isIsogram("hello")); //false
 // console.log(isIsogram("abc")); //true;
