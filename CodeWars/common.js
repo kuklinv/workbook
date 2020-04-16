ffe8a6eca7ab83f3fa44b031803f880ceb308d6f
kuklinv <vitalykuklin@gmail.com>
2020-04-16 22:28:11 +0700

    Feature: responsiblee drinking task = its right solution algoritm with "and" now go to production [ref:no-ref]

--- CodeWars/common.js
+++ CodeWars/common.js
@@ -23,30 +23,79 @@ Output 1:
 //   2 glasses of wine and 1 shot'
 
 function hydrate(s) {
+    console.log(s);
     let sumAlc = 0;
     let glassWord;
     let sArr = s.split(',');
     let noSpacesSArr = [];
     sArr.forEach((item) => noSpacesSArr.push(item.trim()));
-    let routArr = noSpacesSArr.map(item => item.split(' '))
-    // routArr.forEach(function (item) {
-    //   if(!item.some(item => item === 'water')) {
-    //       sumAlc += +item[0];
-    //   }
-    // });
-    routArr.forEach(function (item) {
-            sumAlc += +item[0];
+    console.log(noSpacesSArr);
+    let subArrayforRootArr = [];
+    noSpacesSArr.forEach(function (item) {
+        if (item.indexOf('and') + 1) {
+            console.log(item);
+            let tempSubArray = item.split('and');
+            console.log(tempSubArray);
+            tempSubArray.forEach((item) => subArrayforRootArr.push(item.trim()));
+            noSpacesSArr.splice(noSpacesSArr.indexOf(item), 1);
+        }
+        return subArrayforRootArr;
     });
-    // console.log(`${sumAlc} glasses of water`);
-    if(sumAlc !== 1) {
+    console.log(subArrayforRootArr);
+
+    console.log(noSpacesSArr);
+
+    let rootArr = noSpacesSArr.concat(subArrayforRootArr);
+
+    console.log(rootArr);
+
+    let resArrr = rootArr.map(item => item.split(' '));
+
+    resArrr.forEach(function (item) {
+        if (!item.some(item => item === 'water')) {
+            sumAlc += +item[0];
+        }
+    });
+
+    if (sumAlc !== 1) {
         glassWord = 'glasses';
     } else {
         glassWord = 'glass';
     }
-    return `${sumAlc} ${glassWord} of water`;
+    console.log(`${sumAlc} ${glassWord} of water`);
 }
 
-hydrate('1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer');
+// function hydrateFirstVeersion(s) {
+//     // let separator = new RegExp(/,|and /gm); //
+//     let separator = new RegExp(/\s|and /gm);
+//     let sumAlc = 0;
+//     let glassWord;
+//     let sArr = s.split(',');
+//     let noSpacesSArr = [];
+//     sArr.forEach((item) => noSpacesSArr.push(item.trim()));
+//     console.log(noSpacesSArr);
+//     // let routArr = noSpacesSArr.map(item => item.split(' ')); TODO: jr but need separator ',' or 'and'
+//     let routArr = noSpacesSArr.map(item => item.split(separator));
+//     routArr.forEach(function (item) {
+//         if (!item.some(item => item === 'water')) {
+//             sumAlc += +item[0];
+//         }
+//     });
+//     console.log(routArr);
+//     // routArr.forEach(function (item) {
+//     //         sumAlc += +item[0];
+//     // });
+//     // console.log(`${sumAlc} glasses of water`);
+//     if (sumAlc !== 1) {
+//         glassWord = 'glasses';
+//     } else {
+//         glassWord = 'glass';
+//     }
+//     // return `${sumAlc} ${glassWord} of water`;
+//     console.log(`${sumAlc} ${glassWord} of water`);
+// }
+
+hydrate('1 shot, 5 beers, 2 shots, 1 glass of wine and 1 beer, 1 beer');
 // "10 glasses of water"
 
 ////beginner
