b96ea768a07ccf69ac717fe1fded3923324e1bba
kuklinv <vitalykuklin@gmail.com>
2020-04-16 22:58:21 +0700

    Feature: solved next task [ref:no-ref]

--- CodeWars/common.js
+++ CodeWars/common.js
@@ -1,3 +1,10 @@
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((item, acc) => item + acc);
+}
+
+console.log(arrayPlusArray([1, 2, 3], [3, 2, 1])); //12
+
+
 // function paperwork(n, m) {
 // //     if (n < 0 || m < 0) return 0;
 // // else return n * m;
@@ -18,44 +25,42 @@ Output 1:
 */
 
 //  named =>  Responsible Drinking
-// TODO: i'v not right logic its may be
-//  '1 beer
-//   2 glasses of wine and 1 shot'
 
-function hydrate(s) {
-    console.log(s);
-    let sumAlc = 0;
-    let glassWord;
-    let sArr = s.split(',');
-    let noSpacesSArr = [];
-    sArr.forEach((item) => noSpacesSArr.push(item.trim()));
-    let subArrayforRootArr = [];
-    noSpacesSArr.forEach(function (item) {
-        if (item.indexOf('and') + 1) {
-            let tempSubArray = item.split('and');
-            tempSubArray.forEach((item) => subArrayforRootArr.push(item.trim()));
-            noSpacesSArr.splice(noSpacesSArr.indexOf(item), 1);
-        }
-        return subArrayforRootArr;
-    });
-    let rootArr = noSpacesSArr.concat(subArrayforRootArr);
-    let resArrr = rootArr.map(item => item.split(' '));
 
-    resArrr.forEach(function (item) {
-        if (!item.some(item => item === 'water')) {
-            sumAlc += +item[0];
-        }
-    });
-
-    if (sumAlc !== 1) {
-        glassWord = 'glasses';
-    } else {
-        glassWord = 'glass';
-    }
-    return `${sumAlc} ${glassWord} of water`;
-}
-
-hydrate('1 shot, 5 beers, 2 shots, 1 glass of wine and 1 beer, 1 beer');
+// function hydrate(s) {
+//     console.log(s);
+//     let sumAlc = 0;
+//     let glassWord;
+//     let sArr = s.split(',');
+//     let noSpacesSArr = [];
+//     sArr.forEach((item) => noSpacesSArr.push(item.trim()));
+//     let subArrayforRootArr = [];
+//     noSpacesSArr.forEach(function (item) {
+//         if (item.indexOf('and') + 1) {
+//             let tempSubArray = item.split('and');
+//             tempSubArray.forEach((item) => subArrayforRootArr.push(item.trim()));
+//             noSpacesSArr.splice(noSpacesSArr.indexOf(item), 1);
+//         }
+//         return subArrayforRootArr;
+//     });
+//     let rootArr = noSpacesSArr.concat(subArrayforRootArr);
+//     let resArrr = rootArr.map(item => item.split(' '));
+//
+//     resArrr.forEach(function (item) {
+//         if (!item.some(item => item === 'water')) {
+//             sumAlc += +item[0];
+//         }
+//     });
+//
+//     if (sumAlc !== 1) {
+//         glassWord = 'glasses';
+//     } else {
+//         glassWord = 'glass';
+//     }
+//     return `${sumAlc} ${glassWord} of water`;
+// }
+//
+// hydrate('1 shot, 5 beers, 2 shots, 1 glass of wine and 1 beer, 1 beer');
 // function hydrateFirstVeersion(s) {
 //     // let separator = new RegExp(/,|and /gm); //
 //     let separator = new RegExp(/\s|and /gm);
@@ -65,7 +70,7 @@ hydrate('1 shot, 5 beers, 2 shots, 1 glass of wine and 1 beer, 1 beer');
 //     let noSpacesSArr = [];
 //     sArr.forEach((item) => noSpacesSArr.push(item.trim()));
 //     console.log(noSpacesSArr);
-//     // let routArr = noSpacesSArr.map(item => item.split(' ')); TODO: jr but need separator ',' or 'and'
+//     // let routArr = noSpacesSArr.map(item => item.split(' '));
 //     let routArr = noSpacesSArr.map(item => item.split(separator));
 //     routArr.forEach(function (item) {
 //         if (!item.some(item => item === 'water')) {
@@ -95,3 +100,4 @@ hydrate('1 shot, 5 beers, 2 shots, 1 glass of wine and 1 beer, 1 beer');
 // }
 //
 // console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45)); // true
+
