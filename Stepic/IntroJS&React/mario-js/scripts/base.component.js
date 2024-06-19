43c8f662f645c167cdd16c52620ad369c41c5612
Vitaly <vitalykuklin@gmail.com>
2024-06-19 08:33:48 +0700

    Feature: update base.component [ref:no-ref]

--- Stepic/IntroJS&React/mario-js/scripts/base.component.js
+++ Stepic/IntroJS&React/mario-js/scripts/base.component.js
@@ -1,13 +1,13 @@
-import {PhiramidComponent} from "./phiramid.component";
+import { PhiramidComponent } from "./phiramid.component";
 
 export class BaseComponent {
-    constructor({element}) {
-        this._element = element;
-        this._render();
-    }
+  constructor({ element }) {
+    this.element = element;
+    this.render();
+  }
 
-    _render() {
-        this._element.innerHTML = `
+  render() {
+    this.element.innerHTML = `
     <div class="row">
         <div class="col">
             <h2>Introduction to Java Script and React <span class="badge badge-secondary">STEPIC</span></h2>
@@ -16,16 +16,16 @@ export class BaseComponent {
             <h2>Behold this wonderful pyramid</h2>
         </div>
     </div>
-    <div class="row">
-          <div class="col"></div>
-        <div class="col">
-          <p><button type="button" class="btn btn-secondary btn-sm">Small button</button></p>
-          <p><button type="button" class="btn btn-secondary btn-sm">Small button</button></p>
-        </div>
-    </div>
-    <div class="row">
-         <div class="col"></div>
-    </div>
+    // <div class="row">
+    //       <div class="col"></div>
+    //     <div class="col">
+    //       <p><button type="button" class="btn btn-secondary btn-sm">Small button</button></p>
+    //       <p><button type="button" class="btn btn-secondary btn-sm">Small button</button></p>
+    //     </div>
+    // </div>
+    // <div class="row">
+    //      <div class="col"></div>
+    // </div>
         `;
-    }
+  }
 }
