2e70c7c526e1c04be6bba90adfc679ca020d5435
Vitaly <vitalykuklin@gmail.com>
2025-01-18 12:36:33 +0700

    Feature: add debugger example snippets at DebuggingExpl [ref: no ref]

---
+++ DebuggingExpl/src/counterCache.js
@@ -0,0 +1,12 @@
+export const countCache = {
+    previousCount: 0, 
+    currentCount: 0, 
+    totalCount: 0 
+}
+
+export function updateCache(currentCount, previousCount) {
+    countCache.previousCount = previousCount;
+    countCache.currentCount = currentCount;
+    countCache.totalCount += currentCount;
+}
+
